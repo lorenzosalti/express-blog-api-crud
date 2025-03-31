ee4e0ffbc8fb2b43c17ed29c5acd022b29daa04f
lorenzosalti <lorenzosalti@gmail.com>
2025-03-31 15:58:00 +0200

    feat: bonus 404

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,17 +1,32 @@
 const posts = require('../data/posts.js');
 
 
+
 // index
 function index(req, res) {
   res.json(posts);
 };
 
+
+
 // show
 function show(req, res) {
+
   const postId = parseInt(req.params.id);
   const requiredPost = posts.find(posts => posts.id === postId);
+
+  if (!requiredPost) {
+    return res.status(404).json({
+      status: 404,
+      error: 'Not found',
+      message: 'Post non trovato'
+    });
+  };
+
   res.json(requiredPost);
 };
+
+
 
 // store
 function store(req, res) {
@@ -19,11 +34,15 @@ function store(req, res) {
   res.send('Creazione di un nuovo post');
 };
 
+
+
 // update
 function update(req, res) {
   const postId = req.params.id;
   res.send('Modifica integrale del post ' + postId);
 };
+
+
 
 // modify
 function modify(req, res) {
@@ -31,10 +50,21 @@ function modify(req, res) {
   res.send('Modifica parziale del post ' + postId);
 };
 
+
+
 // destroy
 function destroy(req, res) {
+
   const postId = parseInt(req.params.id);
   const requiredPost = posts.find(posts => posts.id === postId);
+
+  if (!requiredPost) {
+    return res.status(404).json({
+      status: 404,
+      error: 'Not found',
+      message: 'Post non trovato'
+    });
+  };
 
   posts.splice(posts.indexOf(requiredPost), 1);
 
@@ -43,5 +73,7 @@ function destroy(req, res) {
   res.status(204).json(posts);
 
 };
+
+
 
 module.exports = { index, show, store, update, modify, destroy };
