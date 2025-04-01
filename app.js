133b27ee54055c59e43cd3eda6cf36194b3e46a2
lorenzosalti <lorenzosalti@gmail.com>
2025-04-01 18:09:08 +0200

    feat: add middlewares for generic error handling

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const app = express();
 const postsRouter = require('./routers/posts.js');
 const port = 3000;
 
+// middlewares
+const genericError = require('./middlewares/genericError.js');
+const notFound = require('./middlewares/notFound.js');
+
 // public folder
 app.use(express.static('public'));
 
 app.use(express.json());
+
+
 
 // homepage
 app.get('/', (req, res) => {
@@ -16,7 +22,14 @@ app.get('/', (req, res) => {
 // posts router
 app.use('/posts', postsRouter);
 
+
+// middleware for handling errors
+app.use(genericError);
+app.use(notFound);
+
+
 // server listen
 app.listen(port, () => {
   console.log('Server in ascolto alla porta ' + port);
 });
+
